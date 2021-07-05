c03f9fca0d8298f2ddd7dedbb67be930332bbd38
yoshinorin <yoshinorin.net@outlook.com>
2021-07-05 23:10:07 +0900

    add wsl-setup.sh

---
+++ wsl-setup.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+apt update && apt upgrade -y
+apt install -y \
+  bash \
+  curl \
+  coreutils
+  git \
+  grep \
+  iprote2 \
+  iputils-ping \
+  iptables \
+  nginx \
+  traceroute \
+  tcpdump \
+  vim \
+  wget \
+  zip \
+  unzip
+
+# Install Scala & sbt
+curl -s "https://get.sdkman.io?rcupdate=false" | bash && source "$HOME/.sdkman/bin/sdkman-init.sh" && sdk install scala 2.13.4 && sdk install sbt
+
+apt autoremove -y
