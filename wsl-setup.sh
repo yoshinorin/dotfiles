a95a7bb927ed81ba208d7f692db11e03aa32d485
yoshinorin <yoshinorin.net@outlook.com>
2021-07-06 22:33:24 +0900

    updat wsl-setup.sh

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -19,6 +19,6 @@ apt install -y \
   unzip
 
 # Install Scala & sbt
-curl -s "https://get.sdkman.io?rcupdate=false" | bash && source "$HOME/.sdkman/bin/sdkman-init.sh" && sdk install scala 2.13.4 && sdk install sbt
+curl -s "https://get.sdkman.io?rcupdate=false" | bash && source "$HOME/.sdkman/bin/sdkman-init.sh" && sdk install java 11.0.11.hs-adpt && sdk install scala 2.13.4 && sdk install sbt
 
 apt autoremove -y
