0b95a07c4cb49ad55538502b28dbda6b47288f4a
yoshinorin <yoshinorin.net@outlook.com>
2021-07-13 21:08:27 +0900

    Update win-setup.bat

--- win-setup.bat
+++ win-setup.bat
@@ -1,2 +1,2 @@
 choco upgrade chocolatey
-choco install googlechrome firefox git sbt jq wget vscode mariadb wireshark nodejs typescript ruby python golang dotnet awscli -y
+choco install googlechrome firefox make git sbt jq wget vscode mariadb wireshark nodejs typescript ruby python golang dotnet awscli -y
