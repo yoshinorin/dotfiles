9a9b0d4b0b9c4c15eaa7aee9fe954a0b53a70b3b
yoshinorin <yoshinorin.net@outlook.com>
2021-09-03 00:42:05 +0900

    Update win-setup.bat

--- win-setup.bat
+++ win-setup.bat
@@ -2,3 +2,7 @@ choco upgrade chocolatey
 choco install googlechrome firefox make git sbt jq wget vscode mariadb wireshark nodejs typescript ruby python golang dotnet awscli -y
 
 wsl --install
+
+refreshenv
+
+npm install -g hexo-cli
