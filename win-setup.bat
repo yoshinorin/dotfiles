406ca0392981d122e732eca1534c2f7357c9a4bd
yoshinorin <yoshinorin.net@outlook.com>
2021-09-04 00:22:12 +0900

    Update win-setup.bat

--- win-setup.bat
+++ win-setup.bat
@@ -1,7 +1,10 @@
 choco upgrade chocolatey
 choco install googlechrome firefox make git sbt jq wget vscode mariadb wireshark nodejs typescript ruby python golang dotnet awscli -y
 
-wsl --install
+taskkill /f /im OneDrive.exe
+%SystemRoot%\SysWOW64\OneDriveSetup.exe /uninstall
+
+wsl --install -d Ubuntu-20.04
 
 refreshenv
 
