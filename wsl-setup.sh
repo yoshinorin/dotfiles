70b2ef79167a413b2eee6efbed1b2d8636f4f9a5
yoshinorin <yoshinorin.net@outlook.com>
2021-09-04 01:29:09 +0900

    Update wsl-setup.sh

--- wsl-setup.sh
+++ wsl-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+# curl -L https://raw.githubusercontent.com/yoshinorin/dotfiles/master/wsl-setup.sh > wsl-setup.sh
+
 apt update && apt upgrade -y
 apt install -y \
   bash \
@@ -25,5 +27,21 @@ curl -s "https://get.sdkman.io?rcupdate=false" | bash && source "$HOME/.sdkman/b
 
 # Node
 git clone https://github.com/nodenv/nodenv.git ~/.nodenv
+cd ~/.nodenv && src/configure && make -C src
+echo 'export PATH="$HOME/.nodenv/bin:$PATH"' >> ~/.bash_profile
+~/.nodenv/bin/nodenv init
 
+source ~/.bash_profile
+
+curl -fsSL https://github.com/nodenv/nodenv-installer/raw/master/bin/nodenv-doctor | bash
+
+mkdir -p "$(nodenv root)"/plugins
+git clone https://github.com/nodenv/node-build.git "$(nodenv root)"/plugins/node-build
+
+nodenv install 16.8.0
+source ~/.bash_profile
+nodenv global 16.8.0
+
+
+# Clean up
 apt autoremove -y
